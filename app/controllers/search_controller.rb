ddd2fe39905ab347d15e55f1dd57207909353bd7
Paul Rosen <paul@performantsoftware.com>
2011-11-03 14:56:28 +0000

    Back out tanking citations for the moment.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,9 +9,9 @@ class SearchController < ApplicationController
 			is_test = Rails.env == 'test' ? :test : :live
 			is_test = :shards if params[:test_index]
 			# tank citations
-			if query['q']
-				query['q'] = "(#{query['q']} -genre:Citation)^20 (#{query['q']} genre:Citation)^0.1"
-			end
+			#if query['q']
+			#	query['q'] = "(#{query['q']} -genre:Citation)^20 (#{query['q']} genre:Citation)^0.1"
+			#end
 			solr = Solr.factory_create(is_test)
 			@results = solr.search(query)
 
