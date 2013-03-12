a196e42407b5fe4a63d85e54eefc16ad8713b759
Paul Rosen <paul@performantsoftware.com>
2013-03-12 09:03:46 -0400

    Add options to make sendgrid work

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -3,7 +3,7 @@
 CONTACT_US_EMAIL = SITE_SPECIFIC['admin']['email']
 
 Rails.application.config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings[:enable_starttls_auto] = true
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = SITE_SPECIFIC['smtp_settings']['enable_starttls_auto']
 ActionMailer::Base.smtp_settings[:address] = SITE_SPECIFIC['smtp_settings']['address']
 ActionMailer::Base.smtp_settings[:port] = SITE_SPECIFIC['smtp_settings']['port']
 ActionMailer::Base.smtp_settings[:domain] = SITE_SPECIFIC['smtp_settings']['domain']
@@ -11,3 +11,8 @@ ActionMailer::Base.smtp_settings[:user_name] = SITE_SPECIFIC['smtp_settings']['u
 ActionMailer::Base.smtp_settings[:password] = SITE_SPECIFIC['smtp_settings']['password']
 ActionMailer::Base.smtp_settings[:authentication] = SITE_SPECIFIC['smtp_settings']['authentication']
 ActionMailer::Base.default_url_options[:host] = SITE_SPECIFIC['smtp_settings']['return_path']
+
+# This is for sendgrid: it helps with the statistics on the sendgrid site.
+if SITE_SPECIFIC['smtp_settings']['xsmtpapi'].present?
+	ActionMailer::Base.default "X-SMTPAPI" => "{\"category\": \"#{SITE_SPECIFIC['smtp_settings']['xsmtpapi']}\"}"
+end
