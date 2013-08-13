f3a19a79dbe3c6f103856c9b85fca602eb74a69e
Trey Dockendorf <treydock@gmail.com>
2013-08-13 13:08:31 -0500

    - retab using VIM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,41 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery
-	before_filter :dup_params
+  protect_from_forgery
+  before_filter :dup_params
 
-	def must_be_logged_in
-		unless user_signed_in?
-			flash[:alert]  = "Please log in to access that page."
-			redirect_to root_path
-		end
-	end
+  def must_be_logged_in
+    unless user_signed_in?
+      flash[:alert]  = "Please log in to access that page."
+      redirect_to root_path
+    end
+  end
 
-	def render_error(msg, stat = :bad_request)
-		msg = msg[0..msg.index('Backtrace')-1] if msg.include?('Backtrace')
-		@error_msg = msg
-		ActiveRecord::Base.logger.info "**** ERROR: #{@error_msg}"
-		@status = stat
-		@original_request = request.fullpath
-		respond_to do |format|
-			format.html { render :template => '/home/error', :status => @status }
-			format.xml  { render :template => '/home/error', :status => @status }
-		end
-	end
+  def render_error(msg, stat = :bad_request)
+    msg = msg[0..msg.index('Backtrace')-1] if msg.include?('Backtrace')
+    @error_msg = msg
+    ActiveRecord::Base.logger.info "**** ERROR: #{@error_msg}"
+    @status = stat
+    @original_request = request.fullpath
+    respond_to do |format|
+      format.html { render :template => '/home/error', :status => @status }
+      format.xml  { render :template => '/home/error', :status => @status }
+    end
+  end
 
-	def dup_params()
-		if !self.kind_of?(ExhibitsController)
-			# we need to look at the original query string to see if there are any duplicate parameters.
-			# Rails will filter this out so we won't see them if we just look at params[]
-			qs = request.query_string
-			arr = qs.split('&')
-			p = {}
-			arr.each { |q|
-				one = q.split('=')
-				if p[one[0]] == nil
-					p[one[0]] = one[1]
-				else
-					render_error("The parameter \"#{one[0]}\" appears twice in \"#{qs}.\"")
-				end
-			}
-		end
-	end
+  def dup_params()
+    if !self.kind_of?(ExhibitsController)
+      # we need to look at the original query string to see if there are any duplicate parameters.
+      # Rails will filter this out so we won't see them if we just look at params[]
+      qs = request.query_string
+      arr = qs.split('&')
+      p = {}
+      arr.each { |q|
+        one = q.split('=')
+        if p[one[0]] == nil
+          p[one[0]] = one[1]
+        else
+          render_error("The parameter \"#{one[0]}\" appears twice in \"#{qs}.\"")
+        end
+      }
+    end
+  end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,105 +1,105 @@
 class SearchController < ApplicationController
-	# GET /searches
-	# GET /searches.xml
-	def index
-		query_params = QueryFormat.catalog_format()
-		begin
-			QueryFormat.transform_raw_parameters(params)
-			query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
-			is_test = Rails.env == 'test' ? :test : :live
-			is_test = :shards if params[:test_index]
-			# tank citations
-			#if query['q']
-			#	query['q'] = "(#{query['q']} -genre:Citation)^20 (#{query['q']} genre:Citation)^0.1"
-			#end
-			solr = Solr.factory_create(is_test)
-			@results = solr.search(query)
+  # GET /searches
+  # GET /searches.xml
+  def index
+    query_params = QueryFormat.catalog_format()
+    begin
+      QueryFormat.transform_raw_parameters(params)
+      query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
+      is_test = Rails.env == 'test' ? :test : :live
+      is_test = :shards if params[:test_index]
+      # tank citations
+      #if query['q']
+      # query['q'] = "(#{query['q']} -genre:Citation)^20 (#{query['q']} genre:Citation)^0.1"
+      #end
+      solr = Solr.factory_create(is_test)
+      @results = solr.search(query)
 
-			respond_to do |format|
-				format.html # index.html.erb
-				format.xml
-			end
-		rescue ArgumentError => e
-			render_error(e.to_s)
-		rescue SolrException => e
-			render_error(e.to_s, e.status())
-		rescue Exception => e
-			ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
-			render_error("Something unexpected went wrong.", :internal_server_error)
-		end
-	end
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml
+      end
+    rescue ArgumentError => e
+      render_error(e.to_s)
+    rescue SolrException => e
+      render_error(e.to_s, e.status())
+    rescue Exception => e
+      ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
+      render_error("Something unexpected went wrong.", :internal_server_error)
+    end
+  end
 
-	# GET /searches/totals
-	# GET /searches/totals.xml
-	def totals
-		is_test = Rails.env == 'test' ? :test : :live
-		@totals = Solr.get_totals(is_test)
-#		results = [ { :name => 'NINES', :total_docs => 400, :total_archives => 12}, { :name => '18thConnect', :total_docs => 800, :total_archives => 24 } ]
+  # GET /searches/totals
+  # GET /searches/totals.xml
+  def totals
+    is_test = Rails.env == 'test' ? :test : :live
+    @totals = Solr.get_totals(is_test)
+#   results = [ { :name => 'NINES', :total_docs => 400, :total_archives => 12}, { :name => '18thConnect', :total_docs => 800, :total_archives => 24 } ]
 
-		respond_to do |format|
-			format.html # index.html.erb
-			format.xml  # index.xml.builder
-		end
-	end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  # index.xml.builder
+    end
+  end
 
-	# GET /searches/autocomplete
-	# GET /searches/autocomplete.xml
-	def autocomplete
-		query_params = QueryFormat.autocomplete_format()
-		begin
-			QueryFormat.transform_raw_parameters(params)
-			query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
-			query['field'] = "content_auto"
-			is_test = Rails.env == 'test' ? :test : :live
-			is_test = :shards if params[:test_index]
-			solr = Solr.factory_create(is_test)
-			max = query['max'].to_i
-			query.delete('max')
-			words = solr.auto_complete(query)
-			words.sort! { |a,b| b[:count] <=> a[:count] }
-			words = words[0..(max-1)]
-			@results = words.map { |word|
-				{ :item => word[:name], :occurrences => word[:count] }
-			}
+  # GET /searches/autocomplete
+  # GET /searches/autocomplete.xml
+  def autocomplete
+    query_params = QueryFormat.autocomplete_format()
+    begin
+      QueryFormat.transform_raw_parameters(params)
+      query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
+      query['field'] = "content_auto"
+      is_test = Rails.env == 'test' ? :test : :live
+      is_test = :shards if params[:test_index]
+      solr = Solr.factory_create(is_test)
+      max = query['max'].to_i
+      query.delete('max')
+      words = solr.auto_complete(query)
+      words.sort! { |a,b| b[:count] <=> a[:count] }
+      words = words[0..(max-1)]
+      @results = words.map { |word|
+        { :item => word[:name], :occurrences => word[:count] }
+      }
 
-			respond_to do |format|
-				format.html # index.html.erb
-				format.xml
-			end
-		rescue ArgumentError => e
-			render_error(e.to_s)
-		rescue SolrException => e
-			render_error(e.to_s, e.status())
-		rescue Exception => e
-			ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
-			render_error("Something unexpected went wrong.", :internal_server_error)
-		end
-	end
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml
+      end
+    rescue ArgumentError => e
+      render_error(e.to_s)
+    rescue SolrException => e
+      render_error(e.to_s, e.status())
+    rescue Exception => e
+      ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
+      render_error("Something unexpected went wrong.", :internal_server_error)
+    end
+  end
 
-	# GET /searches/names
-	# GET /searches/names.xml
-	def names
-		query_params = QueryFormat.names_format()
-		begin
-			QueryFormat.transform_raw_parameters(params)
-			query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
-			is_test = Rails.env == 'test' ? :test : :live
-			is_test = :shards if params[:test_index]
-			solr = Solr.factory_create(is_test)
-			@results = solr.names(query)
+  # GET /searches/names
+  # GET /searches/names.xml
+  def names
+    query_params = QueryFormat.names_format()
+    begin
+      QueryFormat.transform_raw_parameters(params)
+      query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
+      is_test = Rails.env == 'test' ? :test : :live
+      is_test = :shards if params[:test_index]
+      solr = Solr.factory_create(is_test)
+      @results = solr.names(query)
 
-			respond_to do |format|
-				format.html # index.html.erb
-				format.xml
-			end
-		rescue ArgumentError => e
-			render_error(e.to_s)
-		rescue SolrException => e
-			render_error(e.to_s, e.status())
-		rescue Exception => e
-			ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
-			render_error("Something unexpected went wrong.", :internal_server_error)
-		end
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml
+      end
+    rescue ArgumentError => e
+      render_error(e.to_s)
+    rescue SolrException => e
+      render_error(e.to_s, e.status())
+    rescue Exception => e
+      ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
+      render_error("Something unexpected went wrong.", :internal_server_error)
+    end
   end
 
   # GET /search/languages.xml
@@ -131,30 +131,30 @@ class SearchController < ApplicationController
     end
   end
 
-	# GET /searches/details
-	# GET /searches/details.xml
-	def details
-		query_params = QueryFormat.details_format()
-		begin
-			QueryFormat.transform_raw_parameters(params)
-			query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
-			is_test = Rails.env == 'test' ? :test : :live
-			is_test = :shards if params[:test_index]
-			solr = Solr.factory_create(is_test)
-			@document = solr.details(query)
+  # GET /searches/details
+  # GET /searches/details.xml
+  def details
+    query_params = QueryFormat.details_format()
+    begin
+      QueryFormat.transform_raw_parameters(params)
+      query = QueryFormat.create_solr_query(query_params, params, request.remote_ip)
+      is_test = Rails.env == 'test' ? :test : :live
+      is_test = :shards if params[:test_index]
+      solr = Solr.factory_create(is_test)
+      @document = solr.details(query)
 
-			respond_to do |format|
-				format.html # index.html.erb
-				format.xml
-			end
-		rescue ArgumentError => e
-			render_error(e.to_s)
-		rescue SolrException => e
-			render_error(e.to_s, e.status())
-		rescue Exception => e
-			ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
-			render_error("Something unexpected went wrong.", :internal_server_error)
-		end
-	end
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml
+      end
+    rescue ArgumentError => e
+      render_error(e.to_s)
+    rescue SolrException => e
+      render_error(e.to_s, e.status())
+    rescue Exception => e
+      ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
+      render_error("Something unexpected went wrong.", :internal_server_error)
+    end
+  end
 
 end
