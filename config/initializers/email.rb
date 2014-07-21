b1b4724652f9930aa6bb5e2c84e7519066106c5d
Paul Rosen <paul@paulrosen.net>
2014-07-21 17:45:11 -0400

    Copied email init from another application to see if it fixes the problem.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,15 +1,15 @@
 # Initialize everything about email addresses and sending
 
-CONTACT_US_EMAIL = Rails.application.secrets.admin['email']
 
-Rails.application.config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings[:enable_starttls_auto] = Rails.application.secrets.smtp_settings['enable_starttls_auto']
-ActionMailer::Base.smtp_settings[:address] = Rails.application.secrets.smtp_settings['address']
-ActionMailer::Base.smtp_settings[:port] = Rails.application.secrets.smtp_settings['port']
-ActionMailer::Base.smtp_settings[:domain] = Rails.application.secrets.smtp_settings['domain']
-ActionMailer::Base.smtp_settings[:user_name] = Rails.application.secrets.smtp_settings['user_name']
-ActionMailer::Base.smtp_settings[:password] = Rails.application.secrets.smtp_settings['password']
-ActionMailer::Base.smtp_settings[:authentication] = Rails.application.secrets.smtp_settings['authentication']
+ActionMailer::Base.smtp_settings = {
+	:address => Rails.application.secrets.smtp_settings['address'],
+	:port => Rails.application.secrets.smtp_settings['port'],
+	:domain => Rails.application.secrets.smtp_settings['domain'],
+	:user_name => Rails.application.secrets.smtp_settings['user_name'],
+	:password => Rails.application.secrets.smtp_settings['password'],
+	:authentication => Rails.application.secrets.smtp_settings['authentication'],
+	:enable_starttls_auto => Rails.application.secrets.smtp_settings['enable_starttls_auto']
+}
 ActionMailer::Base.default_url_options[:host] = Rails.application.secrets.smtp_settings['return_path']
 
 # This is for sendgrid: it helps with the statistics on the sendgrid site.
