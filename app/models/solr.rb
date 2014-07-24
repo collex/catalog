f2e88bd27ce8c4aeb467e3c8a6d5986a96027d10
Paul Rosen <paul@paulrosen.net>
2014-07-24 15:37:12 -0400

    Add hasPart and isPartOf

--- app/models/solr.rb
+++ app/models/solr.rb
@@ -37,7 +37,7 @@ class Solr
       "role_DUB", "role_FAC", "role_ILU", "role_ILL", "role_LTG", "role_PRT", "role_POP", "role_PRM",
       "role_RPS", "role_RBR", "role_SCR", "role_SCL", "role_TYD", "role_TYG", "role_WDE", "role_WDC",
 	  "role_BRD", "role_CNG", "role_CND", "role_DRT", "role_IVR", "role_IVE", "role_OWN", "role_FMO", "role_PRF", "role_PRO", "role_PRN",
-      "subject"
+      "subject", "hasPart", "isPartOf"
     ]
 		@facet_fields = ['genre','archive','freeculture', 'has_full_text', 'federation', 'typewright', 'doc_type', 'discipline', 'role']
 	end
